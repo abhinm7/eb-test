553f44737025c16badc1af545e84e48f381f0735
ABHIN M <abhin18m@gmail.com>
2025-05-12 19:24:16 +0530

    env update

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require('express');
+require('dotenv').config();
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, Elastic Beanstalk! :1');
+  res.send('Hello, Elastic Beanstalk!!');
 });
 app.get('/hello', (req, res) => {
   res.send('Hello, Bro');
 });
+app.get('/env',(req,res)=>{
+  let env_text = process.env.SAMPLE_ENV;
+  console.log("env text extracted :",env_text);
+  res.send(env_text)
+})
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
