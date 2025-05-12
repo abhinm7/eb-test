b34a41d5210450e54be19c8222f781424bc9dee5
ABHIN M <abhin18m@gmail.com>
2025-05-12 15:09:43 +0530

    test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, Elastic Beanstalk!');
+  res.send('Hello, Elastic Beanstalk! :1');
 });
 app.get('/hello', (req, res) => {
   res.send('Hello, Bro');
